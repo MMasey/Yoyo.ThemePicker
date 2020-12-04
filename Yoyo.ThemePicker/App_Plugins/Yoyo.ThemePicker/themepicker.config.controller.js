ee621d4dbf61c53f4aa7797f2ee6a666e6daa11d
Mike <mike.masey@yoyodesign.com>
2020-12-04 15:54:56 +0000

    wip: updating theme picker to use file picker for image

--- Yoyo.ThemePicker/App_Plugins/Yoyo.ThemePicker/themepicker.config.controller.js
+++ Yoyo.ThemePicker/App_Plugins/Yoyo.ThemePicker/themepicker.config.controller.js
@@ -1,41 +1,43 @@
-angular.module("umbraco").controller("Yoyo.ThemeConfigController",
-	function ($scope, entityResource, iconHelper, editorService, angularHelper) {
-
+angular
+	.module("umbraco")
+	.controller("Yoyo.ThemePickerConfigController", function (
+		$scope,
+		entityResource,
+		localizationService,
+		iconHelper,
+		editorService,
+		angularHelper
+	) {
 		//NOTE: We need to make each item an object, not just a string because you cannot 2-way bind to a primitive.
 
-		$scope.newItem = {
+		$scope.selectedTheme = {
 			label: "",
-			image: []
+			image: null,
 		};
+
 		$scope.hasError = false;
 		$scope.focusOnNew = false;
-
-		$scope.allowRemove = true;
-		$scope.allowEdit = true;
-		$scope.sortable = false;
-
-		var dialogOptions = {
-			multiPicker: false,
-			entityType: "Media",
-			section: "media",
-			treeAlias: "media",
-			idType: "udi"
-		};
+		$scope.editingIndex = -1;
 
 		if (!angular.isArray($scope.model.value)) {
-
 			//make an array from the dictionary
 			var items = [];
 			for (var i in $scope.model.value) {
 				items.push({
 					value: $scope.model.value[i].value,
 					sortOrder: $scope.model.value[i].sortOrder,
-					id: i
+					id: i,
 				});
 			}
 
 			//ensure the items are sorted by the provided sort order
-			items.sort(function (a, b) { return (a.sortOrder > b.sortOrder) ? 1 : ((b.sortOrder > a.sortOrder) ? -1 : 0); });
+			items.sort(function (a, b) {
+				return a.sortOrder > b.sortOrder
+					? 1
+					: b.sortOrder > a.sortOrder
+					? -1
+					: 0;
+			});
 
 			//now make the editor model the array
 			$scope.model.value = items;
@@ -47,19 +49,19 @@ angular.module("umbraco").controller("Yoyo.ThemeConfigController",
 			$scope.model.value = _.reject($scope.model.value, function (x) {
 				return x.value === item.value;
 			});
-
 		};
 
 		$scope.add = function (evt) {
 			evt.preventDefault();
 
-			if ($scope.newItem) {
-				if (!_.contains($scope.model.value, $scope.newItem)) {
-					$scope.model.value.push({ value: $scope.newItem });
-					$scope.newItem = {
+			if ($scope.selectedTheme) {
+				if (!_.contains($scope.model.value, $scope.selectedTheme)) {
+					$scope.model.value.push({ value: $scope.selectedTheme });
+					$scope.selectedTheme = {
 						label: "",
-						image: []
+						image: null,
 					};
+
 					$scope.hasError = false;
 					$scope.focusOnNew = true;
 					return;
@@ -70,128 +72,72 @@ angular.module("umbraco").controller("Yoyo.ThemeConfigController",
 			$scope.hasError = true;
 		};
 
-		$scope.sortableOptions = {
-			axis: 'y',
-			containment: 'parent',
-			cursor: 'move',
-			items: '> div.control-group',
-			tolerance: 'pointer',
-			update: function (e, ui) {
-				// Get the new and old index for the moved element (using the text as the identifier, so
-				// we'd have a problem if two prevalues were the same, but that would be unlikely)
-				var newIndex = ui.item.index();
-				var movedPrevalueText = $('input[type="text"]', ui.item).val();
-				var originalIndex = getElementIndexByPrevalueText(movedPrevalueText);
-				// Move the element in the model
-				if (originalIndex > -1) {
-					var movedElement = $scope.model.value[originalIndex];
-					$scope.model.value.splice(originalIndex, 1);
-					$scope.model.value.splice(newIndex, 0, movedElement);
-				}
-			}
-		};
-
 		$scope.createNew = function (event) {
 			if (event.keyCode === 13) {
 				$scope.add(event);
 			}
 		};
 
-		function getElementIndexByPrevalueText(value) {
-			for (var i = 0; i < $scope.model.value.length; i++) {
-				if ($scope.model.value[i].value.label === value) {
-					return i;
-				}
-			}
-
-			return -1;
-		}
-
 		// The following handles the media selection
-		$scope.openTreePicker = function () {
-			var treePicker = dialogOptions;
+		$scope.selectImage = function (editingIndex = -1) {
+			localizationService
+				.localize("blockEditor_headlineAddCustomView")
+				.then(function (localizedTitle) {
+					const filePicker = {
+						title: localizedTitle,
+						section: "settings",
+						treeAlias: "files",
+						entityType: "file",
+						isDialog: true,
+						filter: function (i) {
+							return !(
+								i.name.indexOf(".jpg") !== -1 || 
+								i.name.indexOf(".jpeg") !== -1 || 
+								i.name.indexOf(".png") !== -1 || 
+								i.name.indexOf(".svg") !== -1 ||
+								i.name.indexOf(".gif") !== -1 ||
+								i.name.indexOf(".webp") !== -1
+								);
+						},
+						select: function (node) {
+							const filepath = decodeURIComponent(node.id.replace(/\+/g, " "));
 
-			treePicker.submit = function (model) {
-				if (treePicker.multiPicker) {
-					_.each(model.selection, function (item, i) {
-						$scope.addImage(item);
-					});
-				} else {
-					$scope.clearImage();
-					$scope.addImage(model.selection[0]);
-				}
-				editorService.close();
-			};
+							if(editingIndex > -1) {
+								$scope.updateImage(editingIndex, "~/" + filepath);
+							} else {
+								$scope.addImage("~/" + filepath);
+							}
 
-			treePicker.close = function () {
-				editorService.close();
-			};
+							editorService.close();
+						},
+						close: function () {
+							$scope.editingIndex = -1;
 
-			editorService.treePicker(treePicker);
-		};
-
-		$scope.removeImage = function (index) {
-			$scope.newItem.image.splice(index, 1);
-			syncModelValue();
-		};
-
-		$scope.clearImage = function () {
-			$scope.newItem.image = [];
-			syncModelValue();
-		};
-
-		$scope.addImage = function (item) {
-
-			var itemId = dialogOptions.idType === "udi" ? item.udi : item.id;
-
-			var currIds = _.map($scope.newItem.image, function (i) {
-				return dialogOptions.idType === "udi" ? i.udi : i.id;
-			});
-			if (currIds.indexOf(itemId) < 0) {
-
-				item.icon = iconHelper.convertFromLegacyIcon(item.icon);
-				$scope.newItem.image.push({ name: item.name, id: item.id, icon: item.icon, udi: item.udi });
-
-				// store the index of the new item in the newItem.image collection so we can find it again
-				var itemRenderIndex = $scope.newItem.image.length - 1;
-				// get and update the path for the picked node
-				entityResource.getUrl(item.id, dialogOptions.entityType).then(function (data) {
-					$scope.newItem.image[itemRenderIndex].path = data;
+							editorService.close();
+						},
+					};
+					editorService.treePicker(filePicker);
 				});
+		};
 
-			}
-
+		$scope.removeImage = function () {
+			$scope.selectedTheme.image = null;
 			syncModelValue();
+		};
+
+		$scope.addImage = function (filepath) {
+			$scope.selectedTheme.image = filepath;
+			syncModelValue();
+		};
+
+		$scope.updateImage = function (index, filepath) {
+			console.log({...$scope.model})
+			$scope.model.value[index].image = filepath;
+			syncModelValue();
+			console.log("after",{...$scope.model})
 		};
 
 		function syncModelValue() {
-			// var currIds = _.map($scope.newItem.image, function (i) {
-			// 	return dialogOptions.idType === "udi" ? i.udi : i.id;
-			// });
-
-			// console.log(_, $scope.newItem.image, currIds);
-
-			// $scope.model.value = trim(currIds.join(), ",");
 			angularHelper.getCurrentForm($scope).$setDirty();
-		}
-
-		//load media data
-		if ($scope.model.value.length > 0) {
-			var mediaIds = $scope.model.value.map(item => item.value.image[0].id);
-
-			entityResource.getByIds(mediaIds, dialogOptions.entityType).then(function (data) {
-				_.each(data, function (item, i) {
-
-					item.icon = iconHelper.convertFromLegacyIcon(item.icon);
-					// $scope.newItem.image.push({ name: item.name, id: item.id, icon: item.icon, udi: item.udi });
-
-					// // store the index of the new item in the newItem.image collection so we can find it again
-					// var itemRenderIndex = $scope.newItem.image.length - 1;
-					// // get and update the path for the picked node
-					// // entityResource.getUrl(item.id, dialogOptions.entityType).then(function (data) {
-					// // 	$scope.newItem.image[itemRenderIndex].path = data;
-					// // });
-				});
-			});
 		}
 	});
