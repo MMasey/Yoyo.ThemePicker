468eec245134e8170f0f8411442fba7a159b08d6
Mike Masey <mike.masey@yoyodesign.com>
2021-01-22 14:42:04 +0000

    feat: complete new file picker functionality

--- Yoyo.ThemePicker/App_Plugins/Yoyo.ThemePicker/themepicker.config.controller.js
+++ Yoyo.ThemePicker/App_Plugins/Yoyo.ThemePicker/themepicker.config.controller.js
@@ -1,143 +1,165 @@
 angular
 	.module("umbraco")
-	.controller("Yoyo.ThemePickerConfigController", function (
-		$scope,
-		entityResource,
-		localizationService,
-		iconHelper,
-		editorService,
-		angularHelper
-	) {
-		//NOTE: We need to make each item an object, not just a string because you cannot 2-way bind to a primitive.
+	.controller(
+		"Yoyo.ThemePickerConfigController",
+		function (
+			$scope,
+			localizationService,
+			umbRequestHelper,
+			editorService,
+			angularHelper
+		) {
+			//NOTE: We need to make each item an object, not just a string because you cannot 2-way bind to a primitive.
+			$scope.selectedTheme = {
+				label: "",
+				image: null,
+			};
 
-		$scope.selectedTheme = {
-			label: "",
-			image: null,
-		};
+			$scope.hasError = false;
+			$scope.focusOnNew = false;
+			$scope.editingIndex = -1;
 
-		$scope.hasError = false;
-		$scope.focusOnNew = false;
-		$scope.editingIndex = -1;
+			if (!angular.isArray($scope.model.value)) {
+				//make an array from the dictionary
+				// var items = [];
 
-		if (!angular.isArray($scope.model.value)) {
-			//make an array from the dictionary
-			var items = [];
-			for (var i in $scope.model.value) {
-				items.push({
-					value: $scope.model.value[i].value,
-					sortOrder: $scope.model.value[i].sortOrder,
-					id: i,
-				});
+				// for (var i in $scope.model.value) {
+				// 	items.push({
+				// 		value: $scope.model.value[i].value,
+				// 		sortOrder: $scope.model.value[i].sortOrder,
+				// 		id: i,
+				// 	});
+				// }
+
+				// //ensure the items are sorted by the provided sort order
+				// items.sort(function (a, b) {
+				// 	return a.sortOrder > b.sortOrder
+				// 		? 1
+				// 		: b.sortOrder > a.sortOrder
+				// 		? -1
+				// 		: 0;
+				// });
+
+				//now make the editor model the array
+				$scope.model.value = [];
 			}
 
-			//ensure the items are sorted by the provided sort order
-			items.sort(function (a, b) {
-				return a.sortOrder > b.sortOrder
-					? 1
-					: b.sortOrder > a.sortOrder
-					? -1
-					: 0;
-			});
+			$scope.remove = function (item, evt) {
+				evt.preventDefault();
 
-			//now make the editor model the array
-			$scope.model.value = items;
-		}
+				$scope.model.value = _.reject($scope.model.value, function (x) {
+					return x.value === item.value;
+				});
+			};
 
-		$scope.remove = function (item, evt) {
-			evt.preventDefault();
+			$scope.add = function (evt) {
+				if ($scope.selectedTheme) {
+					if (!_.contains($scope.model.value, $scope.selectedTheme)) {
+						$scope.model.value.push({
+							value: $scope.selectedTheme,
+						});
+						$scope.selectedTheme = {
+							label: "",
+							image: null,
+						};
 
-			$scope.model.value = _.reject($scope.model.value, function (x) {
-				return x.value === item.value;
-			});
-		};
-
-		$scope.add = function (evt) {
-			evt.preventDefault();
-
-			if ($scope.selectedTheme) {
-				if (!_.contains($scope.model.value, $scope.selectedTheme)) {
-					$scope.model.value.push({ value: $scope.selectedTheme });
-					$scope.selectedTheme = {
-						label: "",
-						image: null,
-					};
-
-					$scope.hasError = false;
-					$scope.focusOnNew = true;
-					return;
+						$scope.hasError = false;
+						$scope.focusOnNew = true;
+						return;
+					}
 				}
-			}
 
-			//there was an error, do the highlight (will be set back by the directive)
-			$scope.hasError = true;
-		};
+				//there was an error, do the highlight (will be set back by the directive)
+				$scope.hasError = true;
+			};
 
-		$scope.createNew = function (event) {
-			if (event.keyCode === 13) {
-				$scope.add(event);
-			}
-		};
+			$scope.createNew = function (event) {
+				if (event.keyCode === 13) {
+					$scope.add(event);
+				}
+			};
 
-		// The following handles the media selection
-		$scope.selectImage = function (editingIndex = -1) {
-			localizationService
-				.localize("blockEditor_headlineAddCustomView")
-				.then(function (localizedTitle) {
-					const filePicker = {
-						title: localizedTitle,
-						section: "settings",
-						treeAlias: "files",
-						entityType: "file",
-						isDialog: true,
-						filter: function (i) {
-							return !(
-								i.name.indexOf(".jpg") !== -1 || 
-								i.name.indexOf(".jpeg") !== -1 || 
-								i.name.indexOf(".png") !== -1 || 
-								i.name.indexOf(".svg") !== -1 ||
-								i.name.indexOf(".gif") !== -1 ||
-								i.name.indexOf(".webp") !== -1
+			// The following handles the media selection
+			$scope.selectImage = function (editingIndex = -1) {
+				localizationService
+					.localize("blockEditor_headlineAddCustomView")
+					.then(function (localizedTitle) {
+						const filePicker = {
+							title: localizedTitle,
+							section: "settings",
+							treeAlias: "files",
+							entityType: "file",
+							isDialog: true,
+							filterCssClass: "not-allowed",
+							filter: function (i) {
+								return !(
+									i.name.indexOf(".jpg") !== -1 ||
+									i.name.indexOf(".jpeg") !== -1 ||
+									i.name.indexOf(".png") !== -1 ||
+									i.name.indexOf(".svg") !== -1 ||
+									i.name.indexOf(".gif") !== -1 ||
+									i.name.indexOf(".webp") !== -1
 								);
-						},
-						select: function (node) {
-							const filepath = decodeURIComponent(node.id.replace(/\+/g, " "));
+							},
+							select: function (node) {
+								const filepath = decodeURIComponent(
+									node.id.replace(/\+/g, " ")
+								);
 
-							if(editingIndex > -1) {
-								$scope.updateImage(editingIndex, "~/" + filepath);
-							} else {
-								$scope.addImage("~/" + filepath);
-							}
+								if (editingIndex > -1) {
+									$scope.updateImage(
+										editingIndex,
+										"~/" + filepath
+									);
+								} else {
+									$scope.addImage("~/" + filepath);
+								}
 
-							editorService.close();
-						},
-						close: function () {
-							$scope.editingIndex = -1;
+								editorService.close();
+							},
+							close: function () {
+								$scope.editingIndex = -1;
 
-							editorService.close();
-						},
-					};
-					editorService.treePicker(filePicker);
-				});
-		};
+								editorService.close();
+							},
+						};
+						editorService.treePicker(filePicker);
+					});
+			};
 
-		$scope.removeImage = function () {
-			$scope.selectedTheme.image = null;
-			syncModelValue();
-		};
+			$scope.removeImage = function () {
+				$scope.selectedTheme.image = null;
+				syncModelValue();
+			};
 
-		$scope.addImage = function (filepath) {
-			$scope.selectedTheme.image = filepath;
-			syncModelValue();
-		};
+			$scope.addImage = function (filepath) {
+				$scope.selectedTheme.image = getImageFilepath(filepath);
+				syncModelValue();
+			};
 
-		$scope.updateImage = function (index, filepath) {
-			console.log({...$scope.model})
-			$scope.model.value[index].image = filepath;
-			syncModelValue();
-			console.log("after",{...$scope.model})
-		};
+			$scope.updateImage = function (index, filepath) {
+				$scope.model.value[index].value.image = getImageFilepath(
+					filepath
+				);
+				syncModelValue();
+			};
 
-		function syncModelValue() {
-			angularHelper.getCurrentForm($scope).$setDirty();
+			function getImageFilepath(filepath) {
+				if (filepath === null || filepath === "") {
+					return "";
+				}
+				var path = umbRequestHelper.convertVirtualToAbsolutePath(
+					filepath
+				);
+				if (path.toLowerCase().endsWith(".svg") === false) {
+					path += "?upscale=false&width=200";
+				}
+
+				return path;
+			}
+
+			function syncModelValue() {
+				angularHelper.getCurrentForm($scope).$setDirty();
+			}
 		}
-	});
+	);
--- Yoyo.ThemePicker/App_Plugins/Yoyo.ThemePicker/themepicker.controller.js
+++ Yoyo.ThemePicker/App_Plugins/Yoyo.ThemePicker/themepicker.controller.js
@@ -1,33 +1,17 @@
-
-
-angular.module("umbraco").controller("Yoyo.ThemePicker",
-	function ($scope, entityResource) {
-		if ($scope.model.value === undefined || $scope.model.value === null || $scope.model.value === "") {
+angular
+	.module("umbraco")
+	.controller("Yoyo.ThemePicker", function ($scope, entityResource) {
+		if (
+			$scope.model.value === undefined ||
+			$scope.model.value === null ||
+			$scope.model.value === ""
+		) {
 			$scope.model.value = $scope.model.config.themes[0].label;
 		}
 
 		$scope.themes = $scope.model.config.themes;
 
-		$scope.selectTheme = function(theme) {
+		$scope.selectTheme = function (theme) {
 			$scope.model.value = theme.label;
-		}
-
-		if ($scope.themes !== null && $scope.themes.length > 0) {
-
-			var imageIds = $scope.themes.map(t => t.value.image[0].udi) || [];
-
-			if (imageIds.length === $scope.themes.length) {
-
-				entityResource.getByIds(imageIds, "Media").then(function (ent) {
-					for (var i = 0; i < ent.length; i++) {
-						var imageModel = ent[i];
-
-						$scope.themes[i].thumbnail = imageModel.metaData.MediaPath + "?mode=crop&width=200";
-					}
-				});
-			} else {
-				console.warn("The number of them images doesn't match up to the number of available theme options")
-			}
-		}
-
+		};
 	});
